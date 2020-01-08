f1299d4b092a8d93e1955d943d72c9c4346ed98b
Damon Brown <dbrow437@gmail.com>
2020-01-07 19:00:46 -0800

    footer component

--- src/components/header.js
+++ src/components/header.js
@@ -9,10 +9,39 @@ const HeaderLink = props => (
   </Link>
 )
 
+//SocialButton component
+const SocialButton = props => {
+  let style = ""
+  let url = ""
+
+  if (props.site === "instagram") {
+    style = styles.buttonTwitter
+    url = "https://www.instagram.com/" + props.username
+  } else if (props.site === "linkedin") {
+    style = styles.buttonLinkedin
+    url = "https://www.linkedin.com/in/" + props.username
+  } else if (props.site === "github") {
+    style = styles.buttonGithub
+    url = "https://www.github.com/" + props.username
+  }
+
+  return (
+    <a href={url} target="_blank" rel="noopener noreferrer">
+      <div className={style}>{props.children}&nbsp;</div>
+    </a>
+  )
+}
+
 export default () => (
   <header className={styles.container}>
     <div className={styles.row}>
       <Link to="/">My Gatsby blog</Link>
+      <SocialButton site="github" username="dbrow437"></SocialButton>
+      <SocialButton site="linkedin" username="damontbrown"></SocialButton>
+      <SocialButton
+        site="instagram"
+        username="everybodylovedamon"
+      ></SocialButton>
     </div>
 
     <div className={styles.row}>
